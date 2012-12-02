65b969e1ccfee0c73251b20c27a5b4a870e03352
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2012-12-02 16:32:50 +0000

    Ch9 exercise 9

--- src/main/scala/com/aidan/chapter9/Chapter9.scala
+++ src/main/scala/com/aidan/chapter9/Chapter9.scala
@@ -1,7 +1,10 @@
 package com.aidan.chapter9
 
+import java.io.File
+import java.io.PrintWriter
+
+import scala.Array.canBuildFrom
 import scala.io.Source
-import scala.collection.mutable.ArrayBuffer
 
 object Chapter9 {
 
@@ -89,10 +92,22 @@ object Chapter9 {
     // So most of the work has been done by a regular expression...
     fileContents.split("\\s").diff(fps)
   }
-  
+
   def findAllImageSrc(webpageUrl: String) = {
     val rePattern = """(<img) (src=\S*)""".r
-    for ( rePattern(img, src) <- rePattern.findAllIn(Source.fromURL(webpageUrl, "UTF-8").mkString)) yield src.drop(4)
+    for (rePattern(img, src) <- rePattern.findAllIn(Source.fromURL(webpageUrl, "UTF-8").mkString)) yield src.drop(4)
+  }
+
+  /*
+   * Utility function from Ch9 of book
+   */
+  def subdirs(dir: File): Iterator[File] = {
+    val children = dir.listFiles.filter(_.isDirectory())
+    children.toIterator ++ children.toIterator.flatMap(subdirs _)
+  }
+  
+  def countFilesByFileType(directory: String, fileSuffix: String) = {
+    for (d <- subdirs(new File(directory)); f <- d.listFiles if f.toString.endsWith(fileSuffix)) yield f.toString
   }
 
 }
