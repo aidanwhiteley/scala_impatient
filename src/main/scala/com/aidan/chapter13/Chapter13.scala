e28f56faa59f7b851a3447935fa9da0529b07710
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2012-12-17 18:52:36 +0000

    Removed redudant addition into Set

--- src/main/scala/com/aidan/chapter13/Chapter13.scala
+++ src/main/scala/com/aidan/chapter13/Chapter13.scala
@@ -3,9 +3,9 @@ package com.aidan.chapter13
 object Chapter13 {
 	def indexes(aString: String) = {
 	  import scala.collection.mutable
-	  val map = mutable.Map[Char, mutable.Set[Int]]()
-	  for (c <- aString.zipWithIndex) map(c._1) = map.getOrElse(c._1, mutable.Set(c._2)) + c._2
-	  map
+	  val theMap = mutable.Map[Char, mutable.Set[Int]]()
+	  for (c <- aString.zipWithIndex) theMap(c._1) = theMap.getOrElse(c._1, mutable.Set()) + c._2
+	  theMap
 	}
 }
 
