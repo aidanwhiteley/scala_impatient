12c64c5226cce179e60a11c9dc3394ae25a2669e
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2012-12-25 15:05:03 +0000

    Ch14 Ex 2 and 3

--- src/main/scala/com/aidan/chapter14/Chapter14.scala
+++ src/main/scala/com/aidan/chapter14/Chapter14.scala
@@ -1,6 +1,17 @@
 package com.aidan.chapter14
 
 object Chapter14 {
+  def swap(aPair: Tuple2[Int, Int]) = {
+    aPair match {
+      case (x, y) => (y, x)
+    }
+  }
   
+  def swapArray(anArray: Array[Int]) = {
+    anArray match {
+      case Array(x, y, rest @ _*) => Array(y, x) ++ rest
+      case _ => anArray
+    }
+  }
 }
 
--- src/test/scala/com/aidan/chapter14/Chapter14Test.scala
+++ src/test/scala/com/aidan/chapter14/Chapter14Test.scala
@@ -9,9 +9,16 @@ import org.scalatest.junit.JUnitRunner
 class Chapter14Test extends FunSuite {
   import com.aidan.chapter14.Chapter14._
 
-  test("Blah") {
-
+  test("Swap") {
+    val aPair = (5, 8)
+    assert(swap(aPair) === (8, 5))
   }
-
+  
+  test("Swap array values") {
+    assert(swapArray(Array(1, 3, 4, 7)) === Array(3, 1, 4, 7), "Long array")
+    assert(swapArray(Array(1, 3)) === Array(3, 1), "Two array")
+    assert(swapArray(Array(1)) === Array(1), "One array")
+    assert(swapArray(Array()) === Array(), "Zero array")
+  }
 
 }
