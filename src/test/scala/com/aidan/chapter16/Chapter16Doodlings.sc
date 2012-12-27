81a3d36b44fdfdaae7f9ae07aee97adc9ea803e1
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2012-12-27 11:59:27 +0000

    Ch 16 Ex 1 and 2

--- src/test/scala/com/aidan/chapter16/Chapter16Doodlings.sc
+++ src/test/scala/com/aidan/chapter16/Chapter16Doodlings.sc
@@ -2,4 +2,6 @@ package com.aidan.chapter16
 
 object Chapter16Doodlings {
   val doc = <html></html>                         //> doc  : scala.xml.Elem = <html></html>
+  val test = <fred/>(0)                           //> test  : scala.xml.Node = <fred></fred>
+  
 }
--- src/test/scala/com/aidan/chapter16/Chapter16Test.scala
+++ src/test/scala/com/aidan/chapter16/Chapter16Test.scala
@@ -9,10 +9,22 @@ import org.scalatest.junit.JUnitRunner
 class Chapter16Test extends FunSuite {
   import com.aidan.chapter16.Chapter16._
 
-  test("Blah") {
-
+  test("XML literals 1") {
+    val xml1 = <fred/>
+    assert(xml1.getClass === classOf[scala.xml.Elem], "Elem test")
+    // Get first Node in NodeSeq. In worksheeet this returns a Node. Here it returns an Elem????
+    val xml2 = <fred/>(0)
+    assert(xml2.getClass === classOf[scala.xml.Elem], "Elem test 2")
   }
-  
 
+  test("Escaping braces in XML literals") {
+    val braces = <ul>
+                  <li>Opening bracket: [</li>
+                  <li>Closing bracket: ]</li>
+                  <li>Opening brace: {{</li>
+                  <li>Closing brace: }}</li>
+                </ul>
+     assert(braces.getClass() === classOf[scala.xml.Elem])
+  }
 
 }
