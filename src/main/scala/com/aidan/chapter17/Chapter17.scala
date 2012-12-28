e987d4b753b2c375938ea79d0cd90df1fb9ff0a5
Aidan Whiteley <aidantemp-git@yahoo.co.uk>
2012-12-28 16:14:15 +0000

    Ch 17 Ex 3, 4 & 6

--- src/main/scala/com/aidan/chapter17/Chapter17.scala
+++ src/main/scala/com/aidan/chapter17/Chapter17.scala
@@ -2,10 +2,23 @@ package com.aidan.chapter17
 
 object Chapter17 {
   
+  def middle[T](iter: Iterable[T]) = {
+    val midPoint = (iter.size / 2) + 1
+    val result = iter.take(midPoint).last
+    result
+  }
+
 }
 
 class Pair[T, S](val first: T, val second: S) {
-  def swap : Pair[S, T] = new Pair(second, first)
+  def swap: Pair[S, T] = new Pair(second, first)
+}
+
+object Pair {
+  def swap[T, S](aPair: Pair[T, S]): Pair[S, T] = {
+    val pair = new Pair(aPair.second, aPair.first)
+    pair
+  }
 }
 
 class MutablePair[T](var first: T, var second: T) {
@@ -15,7 +28,17 @@ class MutablePair[T](var first: T, var second: T) {
     this.second = temp
     this
   }
+  //def replaceFirst[R >: T](newFirst: R) {
+  //  first = newFirst
+  //}
 }
 
+class Person(val name: String = "Mr Person")  
+class Student(override val name: String = "Mr Student") extends Person
+
+class PairB[T](val first: T, second: T) {
+  def replaceFirst(newFirst: T) = new PairB[T](newFirst, second)
+  def replaceFirstWithSuperType[R >: T](newFirst: R) = new PairB[R](newFirst, second)
+}
 
 
--- src/test/scala/com/aidan/chapter17/Chapter17Test.scala
+++ src/test/scala/com/aidan/chapter17/Chapter17Test.scala
@@ -21,5 +21,35 @@ class Chapter17Test extends FunSuite {
     assert(swapped.second === "Test")
   }
 
+  test("Generic pair awap method") {
+    import com.aidan.chapter17.Pair._
+    
+    val pair = swap(new Pair("A string", 42))
+    assert(pair.first === 42)
+    assert(pair.second === "A string")
+  }
+  
+  test("Testing replaceFirst method and bounds") {
+    val pair1 = new PairB[Student](new Student("Student 1"), new Student("Student 2"))
+    val replacedPair1 = pair1.replaceFirst(new Student("Student 3"))
+    assert(replacedPair1.first.name === "Student 3")
+    
+    val replacedPair2 = pair1.replaceFirstWithSuperType(new Person("Person 1"))
+    assert(replacedPair2.first.name === "Person 1")
+    
+    val pair3 = new PairB[Person](new Person("Person 3"), new Person("Person 4"))
+    val replacedPair3 = pair1.replaceFirst(new Student("Student 4"))
+    // Lower bounds not required as a Student is a Person (allegedly)
+    assert(replacedPair3.first.isInstanceOf[com.aidan.chapter17.Person])
+  }
+  
+  /*
+   * Not worried about defining "middle" for sequences with an even number of elements!
+   */
+  test("Middle element from Iterable[T]") {
+    assert(middle("World") === 'r')
+    val aList = List(1, 2, 3, 4, 5, 6, 7)
+    assert(middle(aList) === 4)
+  }
 
 }
